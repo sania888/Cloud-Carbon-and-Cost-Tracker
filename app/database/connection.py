c29fc6059c415eb9360b04fedacf40804662f1e9
Sania Thapa <saniathapa1970@gmail.com>
2026-05-03 14:13:31 +0530

    adding cloud based database

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/carbon_tracker"
+# Local database URL
+# DATABASE_URL = "postgresql://postgres:1234@localhost:5432/carbon_tracker"
 
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@localhost:5432/carbon_tracker")
 # Create engine
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 # Create session
 SessionsLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
